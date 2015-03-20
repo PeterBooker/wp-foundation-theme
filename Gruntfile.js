41303d66ba642d5c295bebfdc1d4af9558818882
peterbooker <peter@kebopowered.com>
2015-03-20 11:20:26 +0000

    add jshint to deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,6 +102,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
