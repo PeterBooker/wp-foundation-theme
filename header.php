ae1ee7185a848d70551af77d8873fd2239efe2a0
peterbooker <peter@kebopowered.com>
2015-03-22 12:49:17 +0000

    remove site name from header

--- header.php
+++ header.php
@@ -46,7 +46,6 @@
             <div class="small-12 columns">
 
                 <div class="site-branding">
-                    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="Dignity Gaming" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
                     <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
                 </div>
 
