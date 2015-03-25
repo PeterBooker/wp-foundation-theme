27c5ded1b7815782eddd67e6a14800b56056e3c8
peterbooker <peter@kebopowered.com>
2015-03-25 16:07:20 +0000

    foundationify the nav links

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -12,6 +12,7 @@ if ( ! function_exists( 'dignity_paging_nav' ) ) :
  * Display navigation to next/previous set of posts when applicable.
  */
 function foundation_paging_nav() {
+
 	// Don't print empty markup if there's only one page.
 	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
 		return;
@@ -22,16 +23,17 @@ function foundation_paging_nav() {
 		<div class="nav-links">
 
 			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'foundation' ) ); ?></div>
+			<div class="nav-previous"><?php next_posts_link( __( '<span class="button"><i class="fa fa-fw fa-arrow-left"></i>Older posts</span>', 'foundation' ) ); ?></div>
 			<?php endif; ?>
 
 			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'foundation' ) ); ?></div>
+			<div class="nav-next"><?php previous_posts_link( __( '<span class="button">Newer posts <i class="fa fa-fw fa-arrow-right"></i></span>', 'foundation' ) ); ?></div>
 			<?php endif; ?>
 
 		</div><!-- .nav-links -->
 	</nav><!-- .navigation -->
 	<?php
+
 }
 endif;
 
