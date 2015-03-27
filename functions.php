e0aada17cd36230c269f551003fbb4ed0dee1802
peterbooker <peter@kebopowered.com>
2015-03-27 11:33:13 +0000

    set content width to match foundation large screen size

--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ unset( $theme );
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) ) {
-	$content_width = 980; /* pixels */
+	$content_width = 1024; /* pixels */
 }
 
 if ( ! function_exists( 'foundation_setup' ) ) :
