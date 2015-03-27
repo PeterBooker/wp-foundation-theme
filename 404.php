4c5de7be2b11fa44c03cc1a370aa2407cbae881e
peterbooker <peter@kebopowered.com>
2015-03-27 12:09:48 +0000

    formatting

--- 404.php
+++ 404.php
@@ -22,36 +22,49 @@ get_header(); ?>
                         </header><!-- .page-header -->
 
                         <div class="page-content">
+
                             <p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'foundation' ); ?></p>
 
                             <?php get_search_form(); ?>
 
-                            <?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+                            <div class="row">
 
-                            <?php if ( foundation_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-                            <div class="widget widget_categories">
-                                <h2 class="widget-title"><?php _e( 'Most Used Categories', 'foundation' ); ?></h2>
-                                <ul>
-                                <?php
-                                    wp_list_categories( array(
-                                        'orderby'    => 'count',
-                                        'order'      => 'DESC',
-                                        'show_count' => 1,
-                                        'title_li'   => '',
-                                        'number'     => 10,
-                                    ) );
-                                ?>
-                                </ul>
-                            </div><!-- .widget -->
-                            <?php endif; ?>
+                                <div class="small-12 medium-6 large-6 columns">
 
-                            <?php
-                                /* translators: %1$s: smiley */
-                                $archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'foundation' ), convert_smilies( ':)' ) ) . '</p>';
-                                the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-                            ?>
+                                    <?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-                            <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+                                    <?php if ( foundation_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+                                        <div class="widget widget_categories">
+                                            <h2 class="widget-title"><?php _e( 'Most Used Categories', 'foundation' ); ?></h2>
+                                            <ul>
+                                                <?php
+                                                wp_list_categories( array(
+                                                    'orderby'    => 'count',
+                                                    'order'      => 'DESC',
+                                                    'show_count' => 1,
+                                                    'title_li'   => '',
+                                                    'number'     => 10,
+                                                ) );
+                                                ?>
+                                            </ul>
+                                        </div><!-- .widget -->
+                                    <?php endif; ?>
+
+                                </div>
+
+                                <div class="small-12 medium-6 large-6 columns">
+
+                                    <?php
+                                    /* translators: %1$s: smiley */
+                                    $archive_content = '<p>' . __( 'Try looking in the monthly archives:', 'foundation' ) . '</p>';
+                                    the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+                                    ?>
+
+                                    <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+
+                                </div>
+
+                            </div>
 
                         </div><!-- .page-content -->
 
