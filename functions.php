be8ec0711ea2e6a3d80dd8c0442011b5290ffb4e
peterbooker <peter@kebopowered.com>
2015-04-06 14:43:30 +0100

    re-arranging code

--- functions.php
+++ functions.php
@@ -78,6 +78,42 @@ function foundation_setup() {
 		//'default-color' => 'ffffff',
 		//'default-image' => '',
 	//) ) );
+
+    /**
+     * Implement the Custom Header feature.
+     */
+    //require get_template_directory() . '/inc/custom-header.php';
+
+    /**
+     * Custom template tags for this theme.
+     */
+    require get_template_directory() . '/inc/template-tags.php';
+
+    /**
+     * Custom functions that act independently of the theme templates.
+     */
+    require get_template_directory() . '/inc/extras.php';
+
+    /**
+     * Overwrite the Gallery Output - Foundation Clearing
+     */
+    require get_template_directory() . '/inc/gallery.php';
+
+    /**
+     * Customizer additions.
+     */
+    //require get_template_directory() . '/inc/customizer.php';
+
+    /**
+     * Load Jetpack compatibility file.
+     */
+    //require get_template_directory() . '/inc/jetpack.php';
+
+    /**
+     * Custom Foundation Walkers.
+     */
+    require get_template_directory() . '/inc/menu-walkers.php';
+
 }
 endif; // foundation_setup
 add_action( 'after_setup_theme', 'foundation_setup' );
@@ -154,41 +190,6 @@ function foundation_enqueue_files() {
 }
 add_action( 'wp_enqueue_scripts', 'foundation_enqueue_files' );
 add_action( 'admin_enqueue_scripts', 'foundation_enqueue_files' );
-
-/**
- * Implement the Custom Header feature.
- */
-//require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Custom functions that act independently of the theme templates.
- */
-require get_template_directory() . '/inc/extras.php';
-
-/**
- * Overwrite the Gallery Output - Foundation Clearing
- */
-require get_template_directory() . '/inc/gallery.php';
-
-/**
- * Customizer additions.
- */
-//require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-//require get_template_directory() . '/inc/jetpack.php';
-
-/**
- * Custom Foundation Walkers.
- */
-require get_template_directory() . '/inc/menu-walkers.php';
 
 /**
  * Cleanup the WP Head.
